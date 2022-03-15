dd7a010eab035d8d90139f5d5886e07eb52bb497
foreandr <foreandr@gmail.com>
2022-03-15 16:05:12 -0400

    STORED

--- HelperTest/main.py
+++ HelperTest/main.py
@@ -4,18 +4,30 @@ import SQL_CONNECTOR
 def gettingCQAfeeCodes(refno):
     connection = SQL_CONNECTOR.test_connection()
     cursor = connection.cursor()
-    with open('C:\CQA\FULL CQA - DQA/CQAfeecodes.csv', 'w') as file:
-        my_reader = csv.reader(file, delimiter=',')
 
-        #for row in my_reader:
-        #    print(row)
+    query = """SELECT * 
+    FROM env_data env
+    INNER JOIN report rep
+    ON rep.rptno = env.rptno
+    WHERE rep.refno = 'CQA2100100' 
+    """
+    cursor.execute(query)
+    feecode = ''
+    feecodelist = []
+    for item in cursor:
+        # print(item[4], item[6])
+        feecodelist.append([item[4], item[6]])
 
-        query = """SELECT * 
-        FROM env_data env
-        INNER JOIN report rep
-        ON rep.rptno = env.rptno
-        WHERE rep.refno = 'CQA2100540' 
-        """
+    # open and read the file after the appending:
+    with open('C:\CQA\FULL CQA - DQA/CQAfeecodes.csv', 'w') as f:
+        for i in feecodelist:
+            # create the csv writer
+            writer = csv.writer(f)
+
+            # write a row to the csv file
+            writer.writerow(i)
+
+
 
 
 
