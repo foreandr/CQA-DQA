fbd53df5e0da8738deb85ef5f639486d955f0724
foreandr <foreandr@gmail.com>
2022-03-17 09:37:42 -0400

    BARE progress

---
+++ C&DQA/CQAUtilities.py
@@ -0,0 +1,332 @@
+import SQL_CONNECTOR
+import Colors
+import Utilities
+import os, sys
+def OntarioResults(CQARef):
+    print Colors.bcolors.OKCYAN + "\nExecuting Ontario Results" + Colors.bcolors.ENDC
+    # ------------------------------------------------------Env Report------------------------------------------------------#
+    # Dictionary for the environmental report values
+    ENVDict = {
+        "1": "Arsenic",
+        "2": "Cadmium",
+        "3": 'Chromium',
+        "4": 'Cobalt',
+        "5": 'Copper',
+        '6': 'Lead',
+        "7": 'Mercury',
+        '8': 'Molybdenum',
+        '9': 'Nickel',
+        '10': 'Selenium',
+        '11': 'Zinc',
+        '12': 'Total FM > 25 mm',
+        '13': 'Total sharps > 2.8 mm*',
+        '14': 'Total sharps > 12.5 mm',
+        '15': 'Respiration-mgCO2-C/g OM/day',
+        '16': 'E. coli',
+        '17': 'Salmonella spp.',
+        '18': 'Total Organic Matter',
+        '19': 'Moisture',
+        '20': 'Total Organic Matter @ 550 deg C',
+        '22': 'C:N Ratio',
+        '28': 'Total Solids (as received)',
+        '30': 'Bulk Density (As Recieved)',
+        '33': 'Ammonia (NH3/NH4-N)',
+        '34': 'Total Phosphorus (As P205)',
+        '35': 'Total Potassium (as K20)',
+        '36': 'Calcium',
+        '37': 'Magnesium',
+        '38': 'Sulphur',
+        '39': 'Total FM > 2.8 mm*',
+        '40': 'Total plastics > 2.8 mm*'
+    }
+
+    # CoNnEcT To dB
+    cnx = SQL_CONNECTOR.test_connection()
+
+    # Querry out the report numbers
+    cursor = cnx.cursor()
+    query = "SELECT * FROM alms.report WHERE refno='%s'" % (CQARef)
+    cursor.execute(query)
+    reportNumbers = []
+    for item in cursor:
+        # print Colors.bcolors.HEADER + "[Ontario Report REF Query:]" + Colors.bcolors.ENDC + str(item)
+        tempList = [item[0], item[1]]
+        reportNumbers.append(tempList)
+
+    # Store the report numbers
+    soilReport = None
+    envReport = None
+    for item in reportNumbers:
+        if str(item[1]) == "SOIL":
+            soilReport = str(item[0])
+        elif str(item[1]) == "ENVI":
+            envReport = str(item[0])
+
+    # print soilReport, envReport
+
+    ENVResult = {}
+
+    for key in ENVDict.keys():
+        parameter = ENVDict[key]
+        # query every parameter based on name
+        if parameter != "Total FM > 25 mm":
+            # print key, parameter
+            envQuery = r"""select
+            ed.result_str
+            from env_data as ed
+            inner join env_samp as es on ed.rptno=es.rptno and ed.labno=es.labno
+            inner join report as r on ed.rptno=r.rptno
+            inner join feecode as f on ed.feecode=f.feecode and left(f.rpt_dscrpt,4)<>'TCLP' and f.rpt_flag<>'N'
+            left join units as u on ed.unit=u.un_units
+            where (ed.result>=0 or ed.result=-3 or ed.result_str='BDL*') and ed.rptno='%s' and f.rpt_dscrpt='%s'
+            order by f.prt_sort,f.anly_sort""" % (envReport, parameter)
+
+        # 12
+        # query for specific unit of Total FM >25 mm parameter
+        elif parameter == "Total FM > 25 mm":
+            envQuery = r"""select
+                ed.result_str
+                from env_data as ed
+                inner join env_samp as es on ed.rptno=es.rptno and ed.labno=es.labno
+                inner join report as r on ed.rptno=r.rptno
+                inner join feecode as f on ed.feecode=f.feecode and left(f.rpt_dscrpt,4)<>'TCLP' and f.rpt_flag<>'N'
+                left join units as u on ed.unit=u.un_units
+                where (ed.result>=0 or ed.result=-3 or ed.result_str='BDL*') and ed.unit = "pieces/500ml" and ed.rptno='%s' and f.rpt_dscrpt='%s'
+                order by f.prt_sort,f.anly_sort""" % (envReport, parameter)
+
+        cursor = cnx.cursor()
+        cursor.execute(envQuery)
+        # Store the querried information in the ENVResult list
+        for item in cursor:
+            ENVResult[key] = str(item[0])
+        '''
+        print Colors.bcolors.BOLD + Colors.bcolors.UNDERLINE + Colors.bcolors.OKBLUE + \
+              "Key: " + str(key) + " | " + \
+              "Parameter: " + str(parameter) \
+              + "" + Colors.bcolors.ENDC
+        '''
+    #print Colors.bcolors.OKCYAN + 'Ontario----salmonella------' + Colors.bcolors.ENDC
+    #print ENVResult['17']
+    ##    print '=-----moisture------'
+    ##    print ENVResult['19']
+
+    # Querry out moisture to calculate Results as recieved
+    envQuery = r"""select
+            ed.result
+            from env_data as ed
+            inner join env_samp as es on ed.rptno=es.rptno and ed.labno=es.labno
+            inner join report as r on ed.rptno=r.rptno
+            inner join feecode as f on ed.feecode=f.feecode and left(f.rpt_dscrpt,4)<>'TCLP' and f.rpt_flag<>'N'
+            left join units as u on ed.unit=u.un_units
+            where (ed.result>=0 or ed.result=-3 or ed.result_str='BDL*') and ed.rptno='%s' and f.rpt_dscrpt='Moisture'
+            order by f.prt_sort,f.anly_sort""" % (envReport)
+
+    cursor = cnx.cursor()
+    cursor.execute(envQuery)
+    # Store the querried information
+    for item in cursor:
+        moisture = item[0]
+        ENVResult['19'] = moisture
+
+    for i in range(34, 39):
+        temp = str(i)
+        dm = 100 - float(moisture)
+        dm = dm / 100
+        ENVResult[temp] = str(float(ENVResult[temp]) * dm)
+        # ------------------------------------------------------Sieves------------------------------------------------------#
+    # 22
+    # List of all possible Sieve sizes
+    sieveList = [r'Sieve 2 Inch (% Passing)', r'Sieve 1 Inch (% Passing)', r'Sieve 1/2 Inch (% Passing)',
+                 r'Sieve 3/8 Inch (% Passing)',
+                 r'Sieve 1/4 Inch (% Passing)']
+
+    sieveDict = {}
+    sieveResult = {}
+    targetPercent = 80.0
+    smallestParameter = None
+    # Querry out the sieve sizes
+    for item in sieveList:
+        parameter = item
+        envQuery = r"""select
+        ed.result_str
+        from env_data as ed
+        inner join env_samp as es on ed.rptno=es.rptno and ed.labno=es.labno
+        inner join report as r on ed.rptno=r.rptno
+        inner join feecode as f on ed.feecode=f.feecode and left(f.rpt_dscrpt,4)<>'TCLP' and f.rpt_flag<>'N'
+        left join units as u on ed.unit=u.un_units
+        where (ed.result>=0 or ed.result=-3 or ed.result_str='BDL*') and ed.rptno='%s' and f.rpt_dscrpt='%s'
+        order by f.prt_sort,f.anly_sort""" % (envReport, parameter)
+        cursor = cnx.cursor()
+        cursor.execute(envQuery)
+        # Goes through all sieve sizes
+        for item in cursor:
+            a = str(item[0])
+            item = float(item[0])
+            # Check to see if the current sieve size is below the target size and if it is then sets the value too 999
+            if item - targetPercent <= 0:
+                sieveDict[parameter] = 999
+                sieveResult[parameter] = a
+                # If its not below the target size then sets the value to the difference of the two numbers
+            else:
+                sieveDict[parameter] = item - targetPercent
+                sieveResult[parameter] = a
+
+    # get smallest size
+    for key, value in sorted(sieveDict.iteritems(), key=lambda (k, v): (v, k)):
+        smallestParameter = key
+        break
+
+    tempSieve = smallestParameter.replace('Sieve', '').replace('(% Passing)', '')[1:-1]
+
+    # sets the sieve size
+    ENVResult['22'] = tempSieve
+
+    # ------------------------------------------------------Soil Report------------------------------------------------------#
+
+    soilResult = {}
+
+    # Querrys out the different soil report values
+    # 20 and 29: PH
+    soilResult['20'] = Utilities.getPH(CQARef)
+    soilResult['29'] = Utilities.getPH(CQARef)
+
+    # 23: salt
+    query = """select
+        IF(s.salt>0,s.salt,null) as salt
+        from soil as s
+        inner join report as r on r.rptno=s.rptno
+        where s.rptno='%s'
+        order by s.labno""" % (soilReport)
+
+    cursor = cnx.cursor()
+    cursor.execute(query)
+    for item in cursor:
+        soilResult['23'] = str(round(item[0], 1))
+
+    soilResult['32'] = Utilities.getNitrogen(CQARef)
+
+    # -----------Total Organic matter-------------#
+    # print Colors.bcolors.OKCYAN + "----------------------------------Ontario total organic-------------------------------" + Colors.bcolors.ENDC
+
+    soilResult['32'] = Utilities.getNitrogen(CQARef)  # Nitrogen
+    soilResult['18'] = Utilities.getTotalOrganicMatter(CQARef)  # Total organic MAtter
+    available_matter_for_calc = Utilities.getAvailableOrganicMatter(CQARef)
+    totalOrganicCarbon2 = Utilities.organicCarbon(available_matter_for_calc)
+    Nitrogen = float(soilResult['32'])
+    #print 'Total Organic            :' + str(soilResult['18'])
+    #print 'AVailable Organic        : ' + str(available_matter_for_calc)
+    #print 'OG CARB * 0.6            : ' + str(totalOrganicCarbon2)
+    #print 'Nitrogen                 :' + str(Nitrogen)
+
+    # Divide organic carbon by nitrogen
+    CNRatioValue = round((Utilities.organicCarbon(available_matter_for_calc) / 0.9) / Nitrogen)
+    #print 'CNRatioValue             :' + str(CNRatioValue)
+
+    cNRatio = str("%d:1" % (CNRatioValue))
+    # print 'Calculated CN Ratio = ' + cNRatio
+    soilResult['21'] = cNRatio
+    soilResult['31'] = cNRatio
+
+    # ----------------------------Interpretation--------------------------------------------#
+
+    # CEC Calculations (k_m3, mg_m3, ca_m3, na, buffer)
+    # 24-27
+    CECDict = {
+        'k_m3': [],
+        'mg_m3': [],
+        'ca_m3': [],
+        'na': [],
+        'buffer': []
+    }
+    # Querry the cecDict
+    for key in CECDict.keys():
+        parameter = key
+        query = """select
+            IF(s.%s>0,s.%s,null) as %s
+            from soil as s
+            inner join report as r on r.rptno=s.rptno
+            where s.rptno='%s'
+            order by s.labno""" % (parameter, parameter, parameter, soilReport)
+
+        cursor = cnx.cursor()
+        cursor.execute(query)
+        # If they don't exist then stop else add them to the dict
+        for value in cursor:
+            if value is None:
+                sys.exit()
+            else:
+                CECDict[key].append(float(value[0]))
+
+    # perk - k_m3, 390, E28
+    # perMG - _mg_m3, 121.6, E29
+    # perCa - ca_m3, 200.0, E30
+    # perNa - na, 230.0., E26
+
+    # Call the cakculateCEC function
+    cec = Utilities.calculateCEC(CECDict)
+
+    # calculation parameters
+    interpDict = {
+        '24': ['na', 230.0],
+        '25': ['k_m3', 390.0],
+        '26': ['mg_m3', 121.6],
+        '27': ['ca_m3', 200.0]
+    }
+
+    # add results to soilResults
+    for key in interpDict.keys():
+        # call def percentCalc(cec, paramterName, y, CECDict):
+        result = Utilities.percentCalc(cec, CECDict[interpDict[key][0]][0], interpDict[key][1], CECDict)
+        soilResult[key] = result
+
+        # ----------------------------Merging and Formatting--------------------------------------------#
+
+    # Runs function that merges the two dict's
+    tempResult = Utilities.merge_two_dicts(ENVResult, soilResult)
+
+    finalResult = {}
+    # Goes through all the results
+    for key in tempResult.keys():
+        # Stores results into lists
+        value = tempResult[key]
+        digits = Utilities.formatDict[key]
+
+        # Sees what format the number needs and modifies the value to fit the format
+        try:
+            float(value)
+            if digits == 2:
+                finalValue = "%.2f" % (float(value))
+                # print key, value, finalValue
+                finalResult[key] = finalValue
+            elif digits == 1:
+                finalValue = "%.1f" % (float(value))
+                # print key, value, finalValue
+                finalResult[key] = finalValue
+            elif digits == 0:
+                finalValue = int(value)
+                finalResult[key] = finalValue
+            elif digits == '2%':
+                finalValue = "%.2f" % (float(value)) + '%'
+                finalResult[key] = finalValue
+
+        # If there's a value error then don't change it
+        except ValueError:
+            finalResult[key] = value
+
+    cursor.close()
+    cnx.close()
+    print 'Ontario Final Result ' + str(finalResult)
+
+    final_array = []
+    for key in sorted(finalResult):
+        for _key, value in ENVDict.items():
+            if key == _key:
+                # print _key, value, finalResult[key]
+                final_array.append([_key, value, finalResult[key]])
+
+
+    for i in final_array:
+        print(i)
+    return final_array
+
+OntarioResults('CQA2100100')
